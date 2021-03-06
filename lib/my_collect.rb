950f40fa9c68a2dcfa0bc8634cefe9153ba08ea6
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-05 21:49:12 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+    array = []
+    i = 0
+    
+    while i < collection.length
+        yield(collection[i])
+        i += 1
+    end
+      return collection
+end
 
